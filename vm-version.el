e0254a21ade46d3317df60e1845c601491cce388
Manoj Srivastava <srivasta@debian.org>
2003-10-13 07:43:27 +0000

    cscvs to tla changeset 1
    Author: none
    Date: 1997/05/28 17:49:02
    Creation of branch source-dist
    
    git-archimport-id: srivasta@debian.org--2003-primary/vm--upstream--0.1--base-0

---
+++ vm-version.el
@@ -0,0 +1,10 @@
+(provide 'vm)
+
+(provide 'vm-version)
+
+(defconst vm-version "6.31"
+  "Version number of VM.")
+
+(defun vm-version ()
+  "Returns the value of the variable vm-version."
+  vm-version)
